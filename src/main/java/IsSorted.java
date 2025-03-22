9e4d483098a145d66d6c5f0f851221074e8c1229
RazaDon Willrich <razradical93@gmail.com>
2025-03-22 23:33:28 +0000

    Completed Is Sorted.

--- src/main/java/IsSorted.java
+++ src/main/java/IsSorted.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class IsSorted {
     /**
      * Return true if arr is sorted, smallest to largest, false otherwise.
@@ -7,6 +9,20 @@ public class IsSorted {
      * @return true if arr is sorted (all the numbers are in order). false if arr is not sorted.
      */
     public boolean checkSort(int[] arr){
-        return false;
+        
+        boolean sort = true;
+
+        int pointer = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= pointer) {
+                pointer = arr[i];
+            } else {
+                sort = false;
+            }
+        }
+
+        return sort;
+
     }
 }
